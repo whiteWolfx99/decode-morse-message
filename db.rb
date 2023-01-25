02b68bbd0d6f7a5323acf9424c5f4e347c2326b0
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-01-25 16:39:06 -0400

    decode a Morse code character

--- db.rb
+++ db.rb
@@ -1,4 +1,24 @@
+Morse = {
+  'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 
+  'E' => '.', 'F' => '..-.', 'G' => '--.', 'H' => '....',
+  'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
+  'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.',
+  'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-',
+  'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-',
+  'Y' => '-.--', 'Z' => '--..',
+}
 
-puts "hevar"
-puts "nela"
-puts "youssef"
+def decode_char(char)
+  Morse.each do |key, value|
+    if value == char
+      return key
+    end
+  end
+end
+
+puts decode_char('-...')
+
+
+# def from_morse(string)
+#   string.upcase.split('').map { |c| Morse[c] }.join(' ')
+# end
